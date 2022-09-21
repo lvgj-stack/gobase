01cf9911743bc106a1c5b0737d6fcc0269ba5d50
Mr-LvGJ <765171999@qq.com>
2022-09-22 02:52:02 +0800

    feat: info push api

---
+++ pkg/gobase/controller/v1/msg/msg.go
@@ -0,0 +1,49 @@
+package msg
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const (
+	weatherUrl      = "https://restapi.amap.com/v3/weather/weatherInfo?key=a1fbea6fb02c29d25eb8bfd94c854dce&city=310112&extensions=all&output=json"
+	xinZuoUrl       = "https://api.jisuapi.com/astro/fortune?astroid=7&appkey=8fec4338291dc08c"
+	historyTodayUrl = "https://api.jisuapi.com/todayhistory/query?appkey=8fec4338291dc08c&month=1&day=2"
+	xiaoHuaUrl      = "https://api.jisuapi.com/xiaohua/text?pagenum=1&pagesize=1&sort=addtime&appkey=8fec4338291dc08c"
+)
+
+var (
+	weather       = &WeatherResp{}
+	constellation = &Constellation{}
+	tmp           = make(map[string]interface{})
+)
+
+func main() {
+	get(weatherUrl, WEATHER)
+}
+
+func get(url string, typ int) error {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	switch typ {
+	case WEATHER:
+		json.Unmarshal(body, weather)
+	case XINZUO:
+		json.Unmarshal(body, constellation)
+	default:
+		json.Unmarshal(body, tmp)
+	}
+	fmt.Println(string(body))
+	return nil
+}
