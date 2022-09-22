edf86d0db4a2af295a567a581ef6e6ffbd09c1bd
Mr-LvGJ <765171999@qq.com>
2022-09-22 23:33:24 +0800

    fix: add xiao hua

--- pkg/gobase/controller/v1/msg/msg_test.go
+++ pkg/gobase/controller/v1/msg/msg_test.go
@@ -5,8 +5,10 @@ import "testing"
 func TestGetWeather(t *testing.T) {
 	err := get(weatherUrl, WEATHER)
 	get(xinZuoUrl, XINZUO)
+	get(xiaoHuaUrl, XIAOHUA)
 	if err != nil {
 		t.Error(err)
 	}
 	t.Log(constellation.Result.Week.Love)
+	t.Log(xiaoHua.Msg)
 }
